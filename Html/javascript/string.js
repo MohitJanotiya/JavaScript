910fbd5c7490c4a7781f242dcabec4e5f273ca69
Mohit Janotiya <123796513+MohitJanotiya@users.noreply.github.com>
2025-12-27 17:36:21 +0530

    string

---
+++ Html/javascript/string.js
@@ -0,0 +1,11 @@
+let str = "good evening"
+console.log(str)
+console.log(typeof(str))
+console.log(str.length)
+console.log(str.length-1)
+
+
+let d = str.charAt(5)
+console.log(d)
+
+console.log(str.charCodeAt(11))
